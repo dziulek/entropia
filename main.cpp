1bffe299449121630c2dd54d2daf9b25fd4d2b9d
dziulek <99peterstone@gmail.com>
2021-03-22 21:33:28 +0100

    empty commit

--- main.cpp
+++ main.cpp
@@ -1,45 +1,5 @@
 #include "renderer.hpp"
 #include "simulation.hpp"
-<<<<<<< HEAD
-#include "enthropy.hpp"
-#include <iostream>
-
-int main()
-{
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(500, 500), "Enthropy");
-    sf::View view(sf::FloatRect(0,0,500, 500));
-
-    Enthropy enthropy;
-    Simulation sim(window, enthropy, view);
-
-    std::cout<<"siemano"<<std::endl;
-
-    // run the program as long as the window is open
-    while (sim.getWindow()->isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (sim.getWindow()->pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // clear the window with black color
-        sim.getWindow()->clear(sf::Color::Blue);
-
-        // draw everything here...
-        // window.draw(...);
-
-        sim.showView();
-        // end the current frame
-        sim.getWindow()->display();
-        window.setFramerateLimit(60);
-    }
-
-=======
 #include "plot.hpp"
 #include "entropy.hpp"
 #include "stats.hpp"
@@ -126,6 +86,5 @@ int main()
         window.display();
         window.setFramerateLimit(60);
     }
->>>>>>> 633cdf26ec2f1f8263d306ae547f19215966b3bb
     return 0;
 }
